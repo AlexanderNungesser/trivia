0522fd30f9192850cc9dcd1dd550c6313edbbb9b
Alexander Nungesser <Alexander.Nungesser@wago.com>
2026-01-10 14:56:27 +0100

    rename method 'add' to 'addPlayer'

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -31,7 +31,7 @@ public class Game {
 		return (howManyPlayers() >= 2);
 	}
 
-	public boolean add(String playerName) {
+	public boolean addPlayer(String playerName) {
 		
 		
 	    players.add(playerName);
